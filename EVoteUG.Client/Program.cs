f2300ad145ccdf06e8b2d68e3c6c58d11d6d055d
Dxcraig <steviedoe20@gmail.com>
2026-09-21 21:03:51 +0000

    fix: Update HttpClient base address to use configuration setting

--- EVoteUG.Client/Program.cs
+++ EVoteUG.Client/Program.cs
@@ -7,9 +7,10 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+var apiBaseUrl = builder.Configuration["ApiBaseUrl"] ?? "http://localhost:5059/";
 builder.Services.AddScoped(sp => new HttpClient
 {
-    BaseAddress = new Uri("http://localhost:5059/")
+    BaseAddress = new Uri(apiBaseUrl)
 });
 
 builder.Services.AddScoped<ElectionService>();
